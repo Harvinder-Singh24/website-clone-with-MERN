c36e7da8a18035932264b0f49a93a35244e6865b
CHARAN_SIDHU <sidhucharan53@gmail.com>
2021-09-08 11:58:43 +0530

    changes

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Navbar from '../navbar/navbar'
+import './homepage.css'
+
 
 function Homepage() {
-    return (
-      <>
-      <Navbar/>
-        <div>
-          <h1> Hello homepage  </h1>
-
+  return (
+    <>
+      <Navbar />
+      <div className="homepagemaincontainer">
+        <div className="homepagebackground">
+          <div className="headinghomepage">
+            WE ARE STARTUP<span> SPECIALISTS</span></div>
+        <div className="quote">Having successful track record of bringing 500+ products to life.<br />
+          Leading the path of innovation!</div>
         </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default Homepage
